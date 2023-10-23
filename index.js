facc5e596316af416f5270c444ac4d8907f5f7b0
Vincent Tse <river_run@126.com>
2023-10-23 19:30:58 +0800

    🏄‍♂️ Here we go

---
+++ index.js
@@ -0,0 +1,46 @@
+#!/usr/bin/env node
+
+import inquirer from 'inquirer'
+import fs from 'fs'
+
+function createEntity() {
+  const entityName = process.argv[3]
+
+  const entityTemplate = `
+import type { EntitySchema } from '@metaid/metaid'
+
+const ${entityName}Schema: EntitySchema = {
+  name: '${entityName}',
+  nodeName: '', // the name of the brfc node
+  versions: [
+    {
+      version: 1,
+      body: [ // the body of the brfc node
+
+      ]
+    }
+  ]
+}
+
+export default ${entityName}Schema
+`
+  let useDir
+  // 1. determine if user has src folder
+  if (fs.existsSync('./src')) {
+    if (!fs.existsSync('./src/metaid-entities')) {
+      fs.mkdirSync('./src/metaid-entities')
+    }
+    useDir = './src/metaid-entities'
+  } else {
+    if (!fs.existsSync('./metaid-entities')) {
+      fs.mkdirSync('./metaid-entities')
+    }
+    useDir = './metaid-entities'
+  }
+
+  fs.writeFileSync(`${useDir}/${entityName}.entity.ts`, entityTemplate)
+
+  console.log(`🚀 ${entityName}.entity.ts created! 🚀`)
+}
+
+createEntity()
